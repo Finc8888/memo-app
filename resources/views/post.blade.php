f716496ebd37c3fb85590b3aad9ccfcbd0dd21da
friedfox <finc8888@gmail.com>
2024-05-14 21:01:44 +0300

    Scratch 15

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,18 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Blog</title>
-</head>
-<body>
+<x-layout>
     <article>
         <h1>{{ $post->title }}</h1>
         <div>
-        {!! $post->body !!}
+            {!! $post->body !!}
         </div>
     </article>
     <a href="/">Go back</a>
-
-</body>
-</html>
+</x-layout>
